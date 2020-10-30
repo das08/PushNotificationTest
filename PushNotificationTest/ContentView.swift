b2b6ddf1f13b7c164830d7d6e75b900a982c2902
das08 <takeda5955@gmail.com>
2020-10-30 10:40:57 +0900

    Initial Commit

---
+++ PushNotificationTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PushNotificationTest
+//
+//  Created by Kazuki Takeda on 2020/10/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PushNotificationTest/PushNotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationTestApp.swift
+//  PushNotificationTest
+//
+//  Created by Kazuki Takeda on 2020/10/30.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
