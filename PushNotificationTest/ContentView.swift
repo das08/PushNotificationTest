ecc60d221fa5f2f3071f77578ef15d1169d199ff
das08 <takeda5955@gmail.com>
2020-10-30 15:39:22 +0900

    Pushできた！

--- PushNotificationTest/ContentView.swift
+++ PushNotificationTest/ContentView.swift
@@ -2,15 +2,25 @@
 //  ContentView.swift
 //  PushNotificationTest
 //
-//  Created by Kazuki Takeda on 2020/10/30.
+//  Created by das08 on 2020/10/30.
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear(perform: {
+                Messaging.messaging().token { token, error in
+                  if let error = error {
+                    print("Error fetching FCM registration token: \(error)")
+                  } else if let token = token {
+                    print("FCM registration token: \(token)")
+                  }
+                }
+            })
     }
 }
 
@@ -19,3 +29,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
